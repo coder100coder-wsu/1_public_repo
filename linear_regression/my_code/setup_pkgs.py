70f2e073a5b2c8ba37c541621a736a58b64f0b54
coder100coder <94498550+coder100coder-wsu@users.noreply.github.com>
2022-12-20 17:32:21 -0500

    Removed Folder from remote repo

--- linear_regression/my_code/setup_pkgs.py
+++
@@ -1,9 +0,0 @@
-from install_pkgs import import_or_install
-
-def setup_packages(counter=0, bool_var=False, required_pkgs={}):
-    for pkg in required_pkgs:
-        import_or_install(pkg)
-    return 1
-
-# run once to setup required packages in virtual_env
-# setup_packages(bool_var=True)
